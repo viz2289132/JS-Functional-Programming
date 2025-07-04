573c4680c1f5a718b5afde8bbf0b1500f23181e0
Vishal <kumarv8292@gmail.com>
2025-07-04 12:04:28 +0530

    addtodoList

---
+++ 10_JS_Mini_projects/03_Simple_to-do_list/script.js
@@ -0,0 +1,54 @@
+let listContainer = document.querySelector('.listContainer');
+let addInput = document.querySelector('#addInput');
+let addBtn = document.querySelector('.addBtn');
+
+ let list = [];
+
+ 
+
+ function renderList(arr){
+    listContainer.innerHTML = ''
+    arr.map(obj =>{
+        let divElem = document.createElement('div');
+        divElem.classList.add('list');
+        divElem.innerHTML = `
+         <p>${obj.text}</p>
+         
+        `
+        let buttonElem = document.createElement('buttom');
+        buttonElem.innerText = '❌';
+        buttonElem.onclick = ()=>{
+             handleRemoveList(obj.id)
+        }
+        divElem.appendChild(buttonElem);
+        listContainer.append(divElem);
+     })
+ }
+
+ renderList(list);
+
+
+ function handleAddList(){
+
+    let obj = {
+        id : Date.now(),
+        text : addInput.value
+    }
+    list.unshift(obj);
+    renderList(list);
+    addInput.value = '';
+    
+ }
+
+ function handleRemoveList(id){
+    let filteredList = list.filter(obj =>{
+        return obj.id !== id;
+    })
+
+    list = filteredList;
+    renderList(list)
+    
+ }
+
+
+ addBtn.addEventListener('click' , handleAddList);
